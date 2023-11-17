2903661d3df2a74eaa1d8ef62d2f03a625e84b55
Saihan Sadique <saihan.sadique1@gmail.com>
2023-11-17 23:09:17 +0530

    test buttons

---
+++ script.js
@@ -0,0 +1,7 @@
+const smallButton  = document.querySelector("small");
+const mediumButton  = document.querySelector("medium");
+const largeButton  = document.querySelector("large");
+smallButton.addEventListener("onclick")
+{
+    alert("clicked");
+}
