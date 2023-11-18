edbc1e5507a1501d1ad6fadd43a8421121386f22
Saihan Sadique <saihan.sadique1@gmail.com>
2023-11-18 13:27:50 +0530

    ...

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const sizeButtons = document.querySelector(".setting-options");
+const sketchPad = document.querySelector(".sketchPad");
 sizeButtons.addEventListener("click", event=> {
-    if (event.target.textContent == "100x100"){
-        alert("working small");
+    let dimension;
+    if (event.target.textContent == "small"){
+        dimension = 8;
+        populatePad(dimension);
     }
-    else if(event.target.textContent == "200x200"){
-        alert("working medium");
-    }
-    else if(event.target.textContent == "400x400"){
-        alert("working large");
+    else if(event.target.textContent == "medium"){
+        dimension = 4;
+        populatePad(dimension);
+        }
+    else if(event.target.textContent == "large"){
+        dimension = 2;
+        populatePad(dimension);
     }
     else if(event.target.textContent == "Reset pad"){
         alert("working reset pad");
     }
-    
-
 });
-
+populatePad(size){
+    alert(size);
+}
