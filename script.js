2eead1dcd7d2cda0b98f933d2fae6fd6f22072f9
Saihan Sadique <saihan.sadique1@gmail.com>
2023-11-18 12:46:46 +0530

    adding event listeners to the buttons

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const smallButton  = document.querySelector("small");
-const mediumButton  = document.querySelector("medium");
-const largeButton  = document.querySelector("large");
-smallButton.addEventListener("onclick")
-{
-    alert("clicked");
-}
+const sizeButtons = document.querySelector(".setting-options");
+sizeButtons.addEventListener("click", event=> {
+    if (event.target.textContent == "100x100"){
+        alert("working small");
+    }
+    else if(event.target.textContent == "200x200"){
+        alert("working medium");
+    }
+    else if(event.target.textContent == "400x400"){
+        alert("working large");
+    }
+    else if(event.target.textContent == "Reset pad"){
+        alert("working reset pad");
+    }
+    
+
+});
+
